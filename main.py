9b54f987efa0c249dd4673aeb05e2aa7e3cfa7ec
Boyd Peters <boyd.c.peters@gmail.com>
2023-03-27 13:23:52 +0200

    Some code to try things out

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
-import data_processing
+import process_data
 import visualize
 import cs_func
+import models
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-labels, images = data_processing.load_mnist_data("data\mnist_test.csv")
+labels, images = process_data.load_mnist_data("data\mnist_test.csv", normalize=False)
 
-print(labels)
+# print(labels)
 
 a = images[0]
 
 # fig1, ax1 = visualize.plot_image(images[0])
 
-fig1, axs = visualize.plot_images(images[:8])
+# fig1, axs = visualize.plot_images(images[:8])
 
-x = images[0]
-x = x.flatten()
+x_im = images[0]
+x = x_im.flatten()
 
-A = cs_func.create_A(100, 784)
+A = cs_func.create_A(1500, 784)
 y = cs_func.calc_y(A, x)
 print(np.sum(A[:, 0] ** 2))
-print(y)
+# print(y)
+
+x_hat = models.biht(A, y, 400, max_iter=3000, mode="l2", verbose=True)
+
+x_hat = np.reshape(x_hat, (28, 28))
+
+
+# vis = np.concatenate(, axis=2)
+
+# print(vis.shape)
+
+fig2, ax2 = visualize.plot_images((x_im, x_hat))
+plt.show()
 # plt.show()
