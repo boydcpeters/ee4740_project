b9098e1748bcaef37fbb1e08ffe38b64443bf4aa
Boyd Peters <boyd.c.peters@gmail.com>
2023-04-01 11:51:05 +0200

    Updated script to measure mse, nmse and psnr for multiple images over different sparsity levels

--- main_biht.py
+++ main_biht.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import process_data
 import visualize
 import cs_func
@@ -14,7 +16,7 @@ BIHT_CHECK_S_LEVELS_FLAG = True
 
 
 labels, images = process_data.load_mnist_data(
-    "data\mnist_test.csv", normalize=True, max_rows=10
+    "data\mnist_test.csv", normalize=True, max_rows=200
 )
 
 if SPARSITY_DISTRIBUTION_FLAG:
@@ -61,9 +63,37 @@ if BIHT_RUN_FLAG:
 
 
 if BIHT_CHECK_S_LEVELS_FLAG:
-    seed = 1
+
+    def metrics_s_levels_biht(
+        A: np.ndarray, im: np.ndarray, s_level_max: int = 784, verbose: bool = True
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        m, n = A.shape
+
+        x = im.flatten()
+        y = cs_func.calc_y(A, x)
+
+        s_value = min(m, s_level_max)
+        s_levels = np.arange(1, s_value + 1)
+
+        mse = np.zeros(s_levels.shape, dtype=np.float64)
+        nmse = np.zeros(s_levels.shape, dtype=np.float64)
+        psnr = np.zeros(s_levels.shape, dtype=np.float64)
+
+        for i in tqdm(range(s_value)) if verbose else range(s_value):
+            x_hat = models.biht(A, y, (i + 1), max_iter=100, mode="l1", verbose=False)
+            x_hat = np.reshape(x_hat, (28, 28))
+
+            mse[i] = compute_mse(x_im, x_hat)
+            nmse[i] = compute_nmse(x_im, x_hat)
+
+            x_hat_norm = normalize(x_hat)
+            psnr[i] = compute_psnr(x_im, x_hat_norm)
+
+        return s_levels, mse, nmse, psnr
 
     S_LEVEL_MAX = 784
+
+    seed = 1
 
     # Number of measurements
     m = 400
@@ -71,34 +101,43 @@ if BIHT_CHECK_S_LEVELS_FLAG:
     A = cs_func.create_A(m, 784, seed=seed)
     m, n = A.shape
 
-    # Load an image
-    x_im = images[0]
-    x = x_im.flatten()
+    s_value = min(m, S_LEVEL_MAX)
+    s_levels = np.arange(1, s_value + 1)
 
-    y = cs_func.calc_y(A, x)
+    s_levels = np.tile(s_levels, (s_value, 1))
+    mse = np.zeros(s_levels.shape, dtype=np.float64)
+    nmse = np.zeros(s_levels.shape, dtype=np.float64)
+    psnr = np.zeros(s_levels.shape, dtype=np.float64)
 
-    mse = np.zeros(m)
-    nmse = np.zeros(m)
+    for i in tqdm(range(100)):
+        # Load an image
+        x_im = images[i]
 
-    for i in tqdm(range(min(m, S_LEVEL_MAX))):
-        x_hat = models.biht(A, y, (i + 1), max_iter=100, mode="l1", verbose=False)
-        x_hat = np.reshape(x_hat, (28, 28))
+        _, mse[i, :], nmse[i, :], psnr[i, :] = metrics_s_levels_biht(A, x_im)
 
-        mse[i] = compute_mse(x_im, x_hat)
-        nmse[i] = compute_nmse(x_im, x_hat)
+    mse_mean = np.mean(mse, axis=0)
+    mse_std = np.std(mse, axis=0)
 
-    s_levels = np.arange(1, m + 1)
+    nmse_mean = np.mean(nmse, axis=0)
+    nmse_std = np.std(nmse, axis=0)
 
-    fig3, axs3 = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
+    psnr_mean = np.mean(psnr, axis=0)
+    psnr_std = np.std(psnr, axis=0)
 
-    axs3[0].plot(s_levels, mse)
-    axs3[1].plot(s_levels, nmse)
+    fig3, axs3 = plt.subplots(nrows=1, ncols=3, figsize=(12, 4))
+
+    axs3[0].plot(s_levels[0, :], mse_mean)
+    axs3[1].plot(s_levels[0, :], nmse_mean)
+    axs3[2].plot(s_levels[0, :], psnr_mean)
 
     axs3[0].set_xlabel("Sparsity level (s)")
     axs3[0].set_ylabel("MSE")
 
     axs3[1].set_xlabel("Sparsity level (s)")
     axs3[1].set_ylabel("NMSE")
+
+    axs3[2].set_xlabel("Sparsity level (s)")
+    axs3[2].set_ylabel("PSNR")
 
     fig3.tight_layout()
     plt.show()
