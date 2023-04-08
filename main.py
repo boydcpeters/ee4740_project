00175519165bc64a7191807c16f76ba17cdcecb7
Boyd Peters <boyd.c.peters@gmail.com>
2023-04-09 00:26:59 +0200

    Removed almost all code

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import string
+from pathlib import Path
+
 import process_data
 import visualize
 import cs_func
@@ -8,49 +11,159 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-labels, images = process_data.load_mnist_data(
-    "data\\raw\\mnist_test.csv", normalize=True, max_rows=10
-)
+FULL_COMPARISON_ALL = True
 
-a = images[0]
+if FULL_COMPARISON_ALL:
+    path_to_data_processed_biht = f"data\\biht\\metrics_num_m\\processed\\"
+    path_to_data_processed_unnp = f"data\\unnp\\metrics_num_m\\processed\\"
 
-# fig1, ax1 = visualize.plot_image(images[0])
+    if not Path(path_to_data_processed_biht).exists():
+        raise FileNotFoundError(
+            "The data does not exist, first generate/process the data."
+        )
 
-# fig1, axs = visualize.plot_images(images[:8])
+    if not Path(path_to_data_processed_unnp).exists():
+        raise FileNotFoundError(
+            "The data does not exist, first generate/process the data."
+        )
 
-x_im = images[4]
-x = x_im.flatten()
+    # Load all the different data arrays
+    num_m_biht = process_data.load_arr(path_to_data_processed_biht + "num_m.npy")
+    mse_mean_biht = process_data.load_arr(path_to_data_processed_biht + "mse_mean.npy")
+    mse_std_biht = process_data.load_arr(path_to_data_processed_biht + "mse_std.npy")
+    nmse_mean_biht = process_data.load_arr(
+        path_to_data_processed_biht + "nmse_mean.npy"
+    )
+    nmse_std_biht = process_data.load_arr(path_to_data_processed_biht + "nmse_std.npy")
+    psnr_mean_biht = process_data.load_arr(
+        path_to_data_processed_biht + "psnr_mean.npy"
+    )
+    psnr_std_biht = process_data.load_arr(path_to_data_processed_biht + "psnr_std.npy")
 
-A = cs_func.create_A(500, 784, seed=2)
-y = cs_func.calc_y(A, x)
-print(np.sum(A[:, 0] ** 2))
-# print(y)
+    # Load all the different data arrays
+    num_m_unnp = process_data.load_arr(path_to_data_processed_unnp + "num_m.npy")
+    mse_mean_unnp = process_data.load_arr(path_to_data_processed_unnp + "mse_mean.npy")
+    mse_std_unnp = process_data.load_arr(path_to_data_processed_unnp + "mse_std.npy")
+    nmse_mean_unnp = process_data.load_arr(
+        path_to_data_processed_unnp + "nmse_mean.npy"
+    )
+    nmse_std_unnp = process_data.load_arr(path_to_data_processed_unnp + "nmse_std.npy")
+    psnr_mean_unnp = process_data.load_arr(
+        path_to_data_processed_unnp + "psnr_mean.npy"
+    )
+    psnr_std_unnp = process_data.load_arr(path_to_data_processed_unnp + "psnr_std.npy")
 
-# x_hat = models.convex(A, y)
+    # Create the figure
+    fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(4, 10))
 
-# x_hat = models.rfpi(A, y, alpha=0.05, max_iter=100, verbose=True)
-# # TODO: fix l2-mode, it is currently not working
-x_hat = models.biht(A, y, 200, max_iter=3000, mode="l1", verbose=True)
+    # axs[0].errorbar(
+    #     num_m_convex, mse_mean_convex, yerr=mse_std_convex, fmt="--o", capsize=3, label="Convex",
+    # )
+    axs[0].errorbar(
+        num_m_biht,
+        mse_mean_biht,
+        yerr=mse_std_biht,
+        fmt="--o",
+        capsize=3,
+        label="BIHT",
+    )
+    axs[0].errorbar(
+        num_m_unnp,
+        mse_mean_unnp,
+        yerr=mse_std_unnp,
+        fmt="--o",
+        capsize=3,
+        label="UNNP",
+    )
 
-# # print("ADAP")
-# # x_hat_adap = models.biht_adap(A, y, 300, max_iter=3000, mode="l1", verbose=True)
+    axs[0].set_xlabel("Number of measurements (m)")
+    axs[0].set_ylabel("MSE")
+    axs[0].grid(True)
 
-# x_hat = np.reshape(x_hat, (28, 28))
-# # x_hat_adap = np.reshape(x_hat_adap, (28, 28))
+    # axs[1].errorbar(
+    #     num_m_convex, nmse_mean_convex, yerr=nmse_std_convex, fmt="--o", capsize=3, label="Convex",
+    # )
+    axs[1].errorbar(
+        num_m_biht,
+        nmse_mean_biht,
+        yerr=nmse_std_biht,
+        fmt="--o",
+        capsize=3,
+        label="BIHT",
+    )
+    axs[1].errorbar(
+        num_m_unnp,
+        nmse_mean_unnp,
+        yerr=nmse_std_unnp,
+        fmt="--o",
+        capsize=3,
+        label="UNNP",
+    )
+    axs[1].set_xlabel("Number of measurements (m)")
+    axs[1].set_ylabel("NMSE")
+    axs[1].grid(True)
 
+    # axs[2].errorbar(
+    #     num_m_convex, psnr_mean_convex, yerr=psnr_std_convex, fmt="--o", capsize=3, label="Convex",
+    # )
+    axs[2].errorbar(
+        num_m_biht,
+        psnr_mean_biht,
+        yerr=psnr_std_biht,
+        fmt="--o",
+        capsize=3,
+        label="BIHT",
+    )
+    axs[2].errorbar(
+        num_m_unnp,
+        psnr_mean_unnp,
+        yerr=psnr_std_unnp,
+        fmt="--o",
+        capsize=3,
+        label="UNNP",
+    )
+    axs[2].set_xlabel("Number of measurements (m)")
+    axs[2].set_ylabel("PSNR")
+    axs[2].grid(True)
 
-# # vis = np.concatenate(, axis=2)
+    # fig.tight_layout()
 
-# # print(vis.shape)
-x_hat = np.reshape(x_hat, (28, 28))
+    # Remove duplicate labels, adapted from:
+    # https://stackoverflow.com/questions/13588920/stop-matplotlib-repeating-labels-in-legend
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    fig.legend(
+        by_label.values(),
+        by_label.keys(),
+        bbox_to_anchor=(0.5, 0.95),
+        loc="lower center",
+        borderaxespad=0,
+        ncol=3,
+    )
+    # fig.legend(title="Number of measurements (m)", loc=7)
 
-print(f"mse normal: {compute_mse(x_im, x_hat)}")
-print(f"nmse normal: {compute_nmse(x_im, x_hat)}")
+    # # fig.tight_layout()
+    fig.tight_layout(rect=[0.0, 0.0, 1.0, 0.95])
 
+    # Add annotation for subplots
+    for i, ax in enumerate(axs):
+        ax.text(
+            -0.3,
+            1.05,
+            string.ascii_lowercase[i],
+            transform=ax.transAxes,
+            size=16,
+            weight="bold",
+        )
 
-# print(f"mse adap: {compute_mse(x_im, x_hat_adap)}")
-# print(f"nmse adap: {compute_nmse(x_im, x_hat_adap)}")
+    path_savefig = "data\\figures\\"
 
-fig2, ax2 = visualize.plot_images((x_im, x_hat))  # , x_hat_adap))
-plt.show()
-# plt.show()
+    # Create the directory if it does not exist yet
+    if not Path(path_savefig).exists():
+        Path(path_savefig).mkdir(parents=True)
+
+    # if path_savefig is not None:
+    #     fig.savefig(path_savefig + "compare_all.pdf", dpi=200)
+    #     plt.close(fig)
+
+    plt.show()
