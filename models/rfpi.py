0a5c28eeda2ef1483135a7fb56c343639cd6a252
Msabert <81763844+Msabert@users.noreply.github.com>
2023-04-12 00:00:58 +0200

    Add files via upload

--- models/rfpi.py
+++ models/rfpi.py
@@ -2,9 +2,23 @@ import numpy as np
 
 from tqdm import tqdm
 
-
 def negative_func(a):
-    a_hat = (a - np.abs(a)) / 2.0
+
+    """
+    Function preserves all the negative elements and sets all positive elements to 0.
+
+    Parameters
+    ----------
+    a : np.ndarray
+        Array over which the negative function should be taken
+
+    Returns
+    -------
+    np.ndarray
+      Array where all the positive values are set to zero.
+    """
+    
+    a_hat = (a - np.abs(a)) / 2
 
     return a_hat
 
@@ -12,17 +26,45 @@ def negative_func(a):
 def rfpi(
     A: np.ndarray,
     y: np.ndarray,
-    delta=0.01,
-    lam=10,
-    max_iter=20,
-    tol=1e-6,
+    delta=0.1,
+    lam=10000,
+    max_iter=1000,
     verbose: bool = False,
 ):
+    """
+    Function performs the Renormalized fixed point iteration (RFPI) algorithm.
+
+    Parameters
+    ----------
+    A : np.ndarray
+        Measurement matrix (m, n)
+
+    y : np.ndarray
+        Measurement results, sign(Ax) (m x 1)
+
+    delta : int
+        The descent step size
+
+    lam: int
+        The gradient step size
+
+    max_iter : int, optional
+        Maximum number of iterations, by default 1000.
+    
+    verbose: bool, optional
+        Indicates whether the iteration number and the residual error
+        at that iteration should be printed out.
+
+    Returns
+    -------
+    np.ndarray
+        Returns the best x_hat with normalization
+    """
+    #Set the gradient step size
     m, n = A.shape
 
     # Initialize the reconstructed signal x
-    x_hat = np.linalg.pinv(A) @ y
-    # x_hat = np.random.rand(n)
+    x_hat = np.random.rand(n)
     x_hat = x_hat / np.linalg.norm(x_hat)
 
     Y = np.diag(y)
@@ -46,11 +88,6 @@ def rfpi(
         if verbose:
             print(f"Iteration: {i}, ||y - A @ x_hat||_2: {np.linalg.norm(r)}")
 
-        # # Check for convergence and stop iterating if this is the case
-        # if np.linalg.norm(r) < tol:
-        #     print(f"RFPI converged at iteration: {i}")
-        #     break
-
     return x_hat
 
 
@@ -59,11 +96,39 @@ def rfpi_adap(
     y: np.ndarray,
     delta=0.01,
     factor=1.1,
-    threshold=1e-6,
-    max_iter=100,
-    tol=1e-6,
+    max_iter=1000,
     verbose: bool = False,
 ):
+    """
+    Function performs the Renormalized fixed point iteration (RFPI) algorithm.
+
+    Parameters
+    ----------
+    A : np.ndarray
+        Measurement matrix (m, n)
+
+    y : np.ndarray
+        Measurement results, sign(Ax) (m x 1)
+
+    delta : int
+        The descent step size
+
+    factor : int
+        Updata the lam value
+
+    max_iter : int, optional
+        Maximum number of iterations, by default 1000.
+    
+    verbose: bool, optional
+        Indicates whether the iteration number and the residual error
+        at that iteration should be printed out.
+
+    Returns
+    -------
+    np.ndarray
+        Returns the best x_hat based on the best lam
+    """
+
     # Calculate the lowest initial lambda
     lam_init = 100 * delta
 
@@ -72,11 +137,13 @@ def rfpi_adap(
 
     # Create an array to store previous x_hat
     x_hat_prev = np.zeros(A.shape[1], dtype=np.float64)
+    # Set the coverage threshold
+    threshold=1e-6
 
     i = 1
     while True:
         x_hat = rfpi(
-            A, y, delta=delta, lam=lam, max_iter=max_iter, tol=tol, verbose=True
+            A, y, delta=delta, lam=lam, max_iter=max_iter, verbose=True
         )
 
         diff = np.linalg.norm((x_hat - x_hat_prev))
